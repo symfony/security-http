symfony/symfony

Allow disabling redirect on logout

src/Symfony/Component/Security/Http/Tests/Firewall/LogoutListenerTest.php
@@ -122,8 +122,6 @@ class LogoutListenerTest extends TestCase
 
     public function testNoResponseSet()
     {
-        $this->expectException(\RuntimeException::class);
-
         [$listener, , $httpUtils, $options] = $this->getListener();
 
         $request = new Request();
@@ -133,7 +131,11 @@ class LogoutListenerTest extends TestCase
             ->with($request, $options['logout_path'])
             ->willReturn(true);
 
-        $listener(new RequestEvent($this->createMock(HttpKernelInterface::class), $request, HttpKernelInterface::MAIN_REQUEST));
+        $event = new RequestEvent($this->createMock(HttpKernelInterface::class), $request, HttpKernelInterface::MAIN_REQUEST);
+
+        $listener($event);
+
+        $this->assertNull($event->getResponse());
     }
 
     /**

symfony/security-http

Tests/Firewall/LogoutListenerTest.php
@@ -126,13 +126,14 @@ class LogoutListenerTest extends TestCase
 
     public function testNoResponseSet()
     {
-        $listener = $this->getListener();
+        $listener = new LogoutListener($this->tokenStorage, new HttpUtils(), $this->eventDispatcher);
         $request = Request::create('/logout');
-
-        $this->expectException(\RuntimeException::class);
+        $event = new RequestEvent($this->createStub(HttpKernelInterface::class), $request, HttpKernelInterface::MAIN_REQUEST);
 
         $this->assertTrue($listener->supports($request));
-        $listener->authenticate(new RequestEvent($this->createStub(HttpKernelInterface::class), $request, HttpKernelInterface::MAIN_REQUEST));
+        $listener->authenticate($event);
+
+        $this->assertNull($event->getResponse());
     }
 
     #[DataProvider('provideInvalidCsrfTokens')]